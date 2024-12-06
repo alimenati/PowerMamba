25e0415170165544a4fee34c99ba66cb6e3307cf
fatemehdoudi <fatemehdoudi@gmail.com>
2024-12-06 10:24:31 -0600

    Update exp_main.py

--- GridTimes/exp/exp_main.py
+++ GridTimes/exp/exp_main.py
@@ -1,6 +1,6 @@
 from data_provider.data_factory import data_provider
 from exp.exp_basic import Exp_Basic
-from models import TimeMachine, GridTimes, Autoformer, Transformer, DLinear, Linear, NLinear, PatchTST, iTransformer , Informer, TimesNet
+from models import TimeMachine, ElectricMamba, Autoformer, Transformer, DLinear, Linear, NLinear, PatchTST, iTransformer , Informer, TimesNet
 from utils.tools import EarlyStopping, adjust_learning_rate, visual, test_params_flop
 from utils.metrics import metric
 
