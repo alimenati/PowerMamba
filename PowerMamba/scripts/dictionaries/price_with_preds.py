da40f378a2b4592e63f0e5a50945fec4fce27183
fatemehdoudi <fatemehdoudi@gmail.com>
2024-12-09 16:03:47 -0600

    Create price_with_preds.py

---
+++ PowerMamba/scripts/dictionaries/price_with_preds.py
@@ -0,0 +1,7 @@
+project_dict = {
+    'COAST': [241, 97], 'EAST': [242, 49], 'FWEST': [243, 73], 'NORTH': [244, 1], 'NCENT': [245, 25], 'SOUTH': [246, 169], 'SCENT': [247, 121], 'WEST': [248, 145], 'SOLAR': [254, 193], 'WIND': [253, 217]
+}
+
+col_info_dict={
+    'price': [-8, 8], 'wind': [-10, 1], 'solar': [-9, 1], 'anc_serv':[-14, 4], 'load':[-22,8]
+}
