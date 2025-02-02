22b23f07bdaab80f4e644b5138ff47d66a5d9db7
Ali Menati <62931796+alimenati@users.noreply.github.com>
2025-02-02 13:31:36 -0600

    Update data_factory.py

--- PowerMamba/data_provider/data_factory.py
+++ PowerMamba/data_provider/data_factory.py
@@ -2,10 +2,6 @@ from data_provider.data_loader import Dataset_ETT_hour, Dataset_ETT_minute, Data
 from torch.utils.data import DataLoader
 
 data_dict = {
-    'ETTh1': Dataset_ETT_hour,
-    'ETTh2': Dataset_ETT_hour,
-    'ETTm1': Dataset_ETT_minute,
-    'ETTm2': Dataset_ETT_minute,
     'custom': Dataset_Custom,
 }
 
